b9d6ba019fb6582c9da4f7bfb83abe9ec4176f5d
Evan Voeltner <evanvoeltner@gmail.com>
2022-10-28 10:16:39 -0500

    Installed axios + Created starter files + Added MusicTable to app.js

---
+++ src/Components/MusicTable/MusicTable.jsx
@@ -0,0 +1,17 @@
+const MusicTable = (props) => {
+    return ( 
+        <table>
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Artist</th>
+                    <th>Album</th>
+                    <th>Genre</th>
+                    <th>Release Date</th>
+                </tr>
+            </thead>
+        </table>
+     );
+}
+ 
+export default MusicTable;
